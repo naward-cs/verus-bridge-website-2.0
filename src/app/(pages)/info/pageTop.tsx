be9e0706633835e59bbbb5f428b5564bb615a36c
naward-cs <nicholas.ward.cyber@gmail.com>
2024-01-15 09:56:14 -0800

    updated info page content

--- src/app/(pages)/info/pageTop.tsx
+++ src/app/(pages)/info/pageTop.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Image from 'next/image'
-import {Link} from '@nextui-org/react'
+
+import {Icons} from '@/components/shared/icons'
 
 const List = ({text}: {text: string}) => (
   <div className="flex">
     <div className="relative float-left mr-4 mt-1 h-4 w-4 shrink-0">
       <Image src="/images/check-mark-button.png" fill alt="check mark" />
     </div>
-    <p>{text}</p>
+    <p className=" max-w-[456px]">{text}</p>
   </div>
 )
 
@@ -15,40 +16,39 @@ const PageTop = () => {
   return (
     <>
       <div
-        className="h-52 bg-white bg-[length:394px_131px] bg-right-bottom bg-no-repeat px-9 py-8 font-medium shadow-lg md:h-64 md:rounded-xl md:bg-auto md:px-[74px] md:py-16"
+        className="space-y-12 bg-white bg-[length:394px_131px] bg-right-bottom bg-no-repeat px-9 pb-28 pt-8 font-medium shadow-lg  md:rounded-xl md:bg-auto md:px-[74px] md:pb-36 md:pt-[47px]"
         style={{backgroundImage: `url('/images/bridge-img-v2-info.png')`}}
       >
-        <p className="text-[10px] opacity-[37%] md:text-xs  ">
-          FULLY OPERATIONAL SINCE OCT 20, 2023
-        </p>
-        <p className="max-w-[200px] text-xl tracking-tighter text-bluePrimary md:max-w-xs md:text-3xl">
-          The non-custodial & trustless Verus-Ethereum Bridge.
-        </p>
-      </div>
-      <div className="flex flex-col-reverse gap-20 border-[#d1d1d1] px-12 py-14 md:flex-row md:gap-10  md:rounded-xl md:border lg:gap-20 ">
-        <div className="mx-auto max-w-sm md:mx-0">
-          <p>
-            The Verus-Ethereum Bridge allows for the secure transfer and
-            conversion of cryptocurrencies between Verus and Ethereum.
+        <div className="flex flex-col gap-4 md:flex-row md:justify-between">
+          <p className="text-xl tracking-tighter text-bluePrimary  md:text-3xl">
+            Verus-Ethereum Bridge
+            <br />
+            with liquidity pool currency.
           </p>
-          <br />
-          <p>
-            It’s trustless and non-custodial, meaning it doesn’t require users
-            to trust a third party with their funds, and no single entity has
-            control over the assets being transferred.
-          </p>
-          <br />
-          <Link href="https://docs.verus.io/eth-bridge" isExternal>
-            Learn more on docs.verus.io/eth-bridge
-          </Link>
+          <div className="flex  w-fit items-center justify-center gap-[7px] rounded-[10px] border border-[#E3E3E3] bg-[#F9F9F9] p-[10px]  md:gap-[10px]">
+            <Icons.clockInverted
+              height={'24px'}
+              width={'24px'}
+              className="shrink-0"
+            />
+            <p className=" text-[10px] font-medium  leading-3 opacity-[0.37] md:text-xs">
+              FULLY OPERATIONAL
+              <br />
+              SINCE OCT 20, 2023
+            </p>
+          </div>
         </div>
-        <div className="mx-auto flex w-96 shrink-0 flex-col space-y-4  text-base font-medium md:mx-0">
-          <List text="Fully trustless and non-custodial" />
-          <List text="Verified by consensus (miners and stakers)" />
-          <List text="Bridge currency (Bridge.vETH) is a liquidity pool & accrues fees" />
+        <div className="flexshrink-0 mx-auto flex-col space-y-4  text-base font-medium md:mx-0">
+          <List text="Fully trustless, decentralized and non-custodial" />
+          <List text="Bridge crossings are verified by consensus (miners and stakers)" />
           <List
-            text="Bridge to Verus to use native DeFi with low fees (min. 0.025%,
-            max. 0.05%) & L1 security"
+            text="Bridge.vETH is a currency, a liquidity pool AMM & an asset that 
+accrues fees"
+          />
+          <List
+            text="Send Ethereum assets to the Verus network to use native DeFi 
+with low fees (min. 0.025%, max. 0.05%) & L1 security (no smart 
+contract risk)"
           />
         </div>
       </div>
