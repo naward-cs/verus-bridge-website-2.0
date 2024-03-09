29aa2cc8b77ae7d824e12d675c0085f62ed62d75
naward-cs <nicholas.ward.cyber@gmail.com>
2024-03-09 09:24:16 -0800

    clean delgator format keep comments for NFT

--- src/lib/hooks/delegator.ts
+++ src/lib/hooks/delegator.ts
@@ -1,11 +1,11 @@
-'use client' 
+'use client'
 
 import {delegatorABI} from '@/generated'
 import {useQuery} from '@tanstack/react-query'
 import {useContractRead, useContractReads} from 'wagmi'
 import {readContract} from 'wagmi/actions'
 
-// import { abi } from '@/config/abi/DelegatorAbi' 
+// import { abi } from '@/config/abi/DelegatorAbi'
 import DELEGATORABI from '@/config/abi/DelegatorAbiJson.json'
 import {FLAGS} from '@/config/constants'
 import {toBase58Check} from '@/lib/utils/convert'
@@ -13,12 +13,8 @@ import {toBase58Check} from '@/lib/utils/convert'
 import {formatHexAddress} from '../utils/claimUtil'
 import {DelegatorAddress, NetworkChain} from './network'
 
-
-
-
-
 // export const DelegatorAbi = abi
-// import type { Abi } from 'viem' 
+// import type { Abi } from 'viem'
 
 export const useGetTransactionHeight = () => {
   const chainId = NetworkChain()
@@ -94,17 +90,19 @@ export const useGetNFTfromList = () => {
           e.flags & FLAGS.MAPPING_ERC721_NFT_DEFINITION
             ? '[ERC721]'
             : e.flags & FLAGS.MAPPING_ERC1155_NFT_DEFINITION
-            ? '[ERC1155 Verus single NFT]'
-            : '[ERC1155]',
+              ? '[ERC1155 Verus single NFT]'
+              : '[ERC1155]',
         iaddress: e.iaddress,
         erc20address: e.erc20ContractAddress,
         value: e.tokenID,
         flags: e.flags,
       }))
+
       // const ercList = list.filter(
       //   (token) => token.flags & FLAGS.MAPPING_ERC20_DEFINITION && token.label
       // )
       // return ercList as TokenList[]
+      
       return list.filter(
         (nft) =>
           nft.flags &
